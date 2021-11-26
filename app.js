835a2542a89b3e9903190ace4cfa7dd96c045336
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-11-26 22:54:02 +0530

    done

--- app.js
+++ app.js
@@ -5,35 +5,50 @@ const number=document.querySelector("#number");
 
 submitbtn.addEventListener("click",checker);
 
-pswrd.addEventListener("input", copy);
-    function copy()
-    {
-        let Orignalvalue=pswrd.value;
-        return Orignalvalue;
-    }
+function outputMsg(message)
+{   number.innerHTML=message;
+}
 function checker()
 {
-        let Ovalue=copy();
-        let splitted=Ovalue.split();
-        let count=0;
-        for(i=0; i<10;i++)
-        {
-            splitted[i];
-            count=count+1;
-        }
-        if(count<10)
-        {
-            number.innerHTML="Enter 10 Characters";
-        }
-        else if (count==10)
-         {
-            number.innerHTML="Perfecto";
+    let output=pswrd.value;
+    let passwordsplit=output.length;
+    if(passwordsplit==10)
+    {
+        outputMsg('Password is successful.');
+        pswrd.style.background="green";
+    }
+    else if(passwordsplit<10)
+    {
+        outputMsg('Password is not successful.');
+        pswrd.style.background="red";
+    }
+    else(passwordsplit>10)
+    {
+        outputMsg('Password is not successful.');
+        pswrd.style.background="red";
+    }
+    
+        // let Ovalue=pswrd.value;
+        // let splitted=Ovalue.split();
+        // let count=0;
+        // for(i=0; i<10;i++)
+        // {
+        //     splitted[i];
+        //     count=count+1;
+        // }
+        // if(count<10)
+        // {
+        //     number.innerHTML="Enter 10 Characters";
+        // }
+        // else if (count==10)
+        //  {
+        //     number.innerHTML="Perfecto";
 
-        }
-        else
-        {
-            number.innerHTML="Enter only 10 characters";
-        }
+        // }
+        // else
+        // {
+        //     number.innerHTML="Enter only 10 characters";
+        // }
     
 
 }
